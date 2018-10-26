7ffdcca8e98cf85bba810a4a9de9aaea225cd627
anibalsolon <anibalsolon@gmail.com>
2018-10-25 21:54:48 -0400

    fix icons

--- app/components/icons.js
+++ app/components/icons.js
@@ -1,53 +1,44 @@
 import React, { Component } from 'react'
 
-export {
-  DeviceHub as PipelineIcon,
-  Accessibility as SubjectIcon,
-  Accessibility as ParticipantIcon,
-  Accessibility as DatasetIcon,
-  ExpandMore as ExpandMoreIcon,
-  MoreVert as MoreVertIcon,
-  OpenInNew as OpenIcon,
-  Launch as LaunchIcon,
-  RemoveCircle as RemoveIcon,
-  NavigateNext as NavigateNextIcon,
-  Settings as SettingsIcon,
-  DeveloperBoard as EnvironmentIcon,
-  PlayCircleFilled as RunIcon,
-  PauseCircleFilled as RunPausedIcon,
-  Folder as ProjectIcon,
-  FolderOpen as ProjectOpenIcon,
-  MenuIcon as Menu,
-  Extension as PipelineStepIcon,
-  AccessTime as PipelineExecutionTimeIcon,
-
-  LoadingIcon as Loop,
-  AuthErrorIcon as Lock,
-  ErrorIcon as Error,
-  DoneIcon as Done,
-  Timer as TimerIcon,
-  Note as LogIcon,
-
-  Cloud as EnvAwsIcon,
-  Laptop as EnvLocalIcon,
-  SettingsRemote as EnvApiIcon,
-  Link as EnvSshIcon,
-  Devices as EnvDockerIcon,
-
-  Home as HomeIcon,
-  KeyboardArrowRight as NextIcon,
-
-  CloudDownload as DownloadIcon,
-  Save as SaveIcon,
-  Cached as RevertIcon,
-} from '@material-ui/icons';
+export { default as PipelineIcon } from '@material-ui/icons/DeviceHub';
+export { default as SubjectIcon } from '@material-ui/icons/Accessibility';
+export { default as ParticipantIcon } from '@material-ui/icons/Accessibility';
+export { default as DatasetIcon } from '@material-ui/icons/Accessibility';
+export { default as ExpandMoreIcon } from '@material-ui/icons/ExpandMore';
+export { default as MoreVertIcon } from '@material-ui/icons/MoreVert';
+export { default as OpenIcon } from '@material-ui/icons/OpenInNew';
+export { default as LaunchIcon } from '@material-ui/icons/Launch';
+export { default as RemoveIcon } from '@material-ui/icons/RemoveCircle';
+export { default as NavigateNextIcon } from '@material-ui/icons/NavigateNext';
+export { default as SettingsIcon } from '@material-ui/icons/Settings';
+export { default as EnvironmentIcon } from '@material-ui/icons/DeveloperBoard';
+export { default as RunIcon } from '@material-ui/icons/PlayCircleFilled';
+export { default as RunPausedIcon } from '@material-ui/icons/PauseCircleFilled';
+export { default as ProjectIcon } from '@material-ui/icons/Folder';
+export { default as ProjectOpenIcon } from '@material-ui/icons/FolderOpen';
+export { default as MenuIcon } from '@material-ui/icons/Menu';
+export { default as PipelineStepIcon } from '@material-ui/icons/Extension';
+export { default as PipelineExecutionTimeIcon } from '@material-ui/icons/AccessTime';
+export { default as LoadingIcon } from '@material-ui/icons/Loop';
+export { default as AuthErrorIcon } from '@material-ui/icons/Lock';
+export { default as ErrorIcon } from '@material-ui/icons/Error';
+export { default as DoneIcon } from '@material-ui/icons/Done';
+export { default as TimerIcon } from '@material-ui/icons/Timer';
+export { default as LogIcon } from '@material-ui/icons/Note';
+export { default as EnvAwsIcon } from '@material-ui/icons/Cloud';
+export { default as EnvLocalIcon } from '@material-ui/icons/Laptop';
+export { default as EnvApiIcon } from '@material-ui/icons/SettingsRemote';
+export { default as EnvSshIcon } from '@material-ui/icons/Link';
+export { default as EnvDockerIcon } from '@material-ui/icons/Devices';
+export { default as HomeIcon } from '@material-ui/icons/Home';
+export { default as NextIcon } from '@material-ui/icons/KeyboardArrowRight';
+export { default as DownloadIcon } from '@material-ui/icons/CloudDownload';
+export { default as SaveIcon } from '@material-ui/icons/Save';
+export { default as RevertIcon } from '@material-ui/icons/Cached';
+export { default as BrainIcon } from '@material-ui/icons/Image';
 
 // import createSvgIcon from '../utils/createSvgIcon'
-
 // export const BrainIcon = createSvgIcon(
 //   <g><path d="M22 5.72l-4.6-3.86-1.29 1.53 4.6 3.86L22 5.72zM7.88 3.39L6.6 1.86 2 5.71l1.29 1.53 4.59-3.85zM12.5 8H11v6l4.75 2.85.75-1.23-4-2.37V8zM12 4c-4.97 0-9 4.03-9 9s4.02 9 9 9c4.97 0 9-4.03 9-9s-4.03-9-9-9zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z" /></g>
 // , 'BrainIcon');
-
-export {
-  Image as BrainIcon,
-} from '@material-ui/icons';
+// https://www.flaticon.com/family/detailed-rounded/lineal
