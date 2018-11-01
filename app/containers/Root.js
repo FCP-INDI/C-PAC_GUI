99c8c59eaa709623afac870d52618b7969cb1963
anibalsolon <anibalsolon@gmail.com>
2018-11-01 12:44:40 -0400

    change theme color to "teal"

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -4,11 +4,16 @@ import { ConnectedRouter } from 'react-router-redux';
 import Routes from '../routes';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
 
 const theme = createMuiTheme({
   typography: {
     useNextVariants: true,
   },
+  palette: {
+    primary: { main: teal[200] },
+    secondary: { main: teal[600] }
+  }
 });
 
 export default class Root extends Component {
