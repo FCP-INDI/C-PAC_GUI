fbbc5cffd7cfd756e3ba9f7078911b142ec34f35
anibalsolon <anibalsolon@gmail.com>
2019-05-10 19:28:26 -0400

    initial cloud front configuration

---
+++ tools/data/aws/main.tf
@@ -0,0 +1,50 @@
+locals {
+  domain = "bucket.test.com"
+}
+
+data "aws_s3_bucket" "selected" {
+  bucket = "${locals.domain}"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.b.bucket_regional_domain_name}"
+    origin_id   = "${local.bucket}"
+
+    s3_origin_config {
+      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = ["${locals.domain}]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${local.bucket}"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_All"
+
+  viewer_certificate {
+    # Recheck
+    cloudfront_default_certificate = true
+  }
+}
