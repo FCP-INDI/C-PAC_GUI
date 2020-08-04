9293fb7f547d21bc8816add01a1d08cc78033622
Anibal Solon <anibalsolon@gmail.com>
2020-08-03 21:00:37 -0400

    theme adjustments

--- app/theme.js
+++ app/theme.js
@@ -4,6 +4,7 @@ import teal from '@material-ui/core/colors/teal';
 export default createMuiTheme({
   typography: {
     useNextVariants: true,
+    fontSize: 15,
   },
   palette: {
     primary: { main: teal[200] },
@@ -22,7 +23,75 @@ export default createMuiTheme({
       adornedEnd: {
         // paddingRight: 0,
         width: 'calc(100% - 14px)',
+      },
+    },
+    MuiButton: {
+      root: {
+        whiteSpace: 'nowrap',
+      },
+    },
+    MuiAlert: {
+      root: {
+        lineHeight: 2.85,
+        padding: '6px 16px',
+        minHeight: 'calc(2.85em + 12px)',
+        '@media (max-width:700px)': {
+          flexWrap: 'wrap',
+        },
+      },
+      icon: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        '@media (max-width:700px)': {
+          flexBasis: '10%',
+        },
+      },
+      message: {
+        lineHeight: 'normal',
+        justifyContent: 'left',
+        alignSelf: 'center',
+        '@media (max-width:700px)': {
+          flexBasis: '80%',
+          textAlign: 'center',
+        },
+      },
+      action: {
+        lineHeight: 'normal',
+        display: 'block',
+        alignSelf: 'center',
+        '@media (max-width:700px)': {
+          paddingLeft: 0,
+          margin: '5px auto',
+        },
+      },
+    },
+    MuiSelect: {
+      select: {
+        '&:focus': {
+          backgroundColor: 'inherit',
+        }
+      },
+    },
+    MuiStepper: {
+      root: {
+        '&$vertical': {
+          '@media (max-width:600px)': {
+            padding: 0.
+          },
+        },
+      },
+    },
+    MuiListItemIcon: {
+      root: {
+        minWidth: 32
       }
+    },
+    MuiBadge: {
+      root: {
+        "& .MuiSvgIcon-root": {
+          fontSize: '1.4em',
+        }
+      },
     },
     MuiFormGroup: {
       root: {
