c529cf4cb174cb5ea730db1bf2acc79ed009083a
yiran <yiransucdr@utexas.edu>
2021-03-22 13:04:57 -0500

    Fix: added back schedulerSelector to Breadcrumbs.js (header)

--- app/reducers/cpacpy.js
+++ app/reducers/cpacpy.js
@@ -26,7 +26,7 @@ const initialState = fromJS({
     { id: 'local', name: 'Local', version: 'unknown', backends: [], address: scheduler.local, polling: false, detecting: false, online: null, connected: false, connect: { callbacks: {} } },
   ],
   // id for the latest used scheduler
-  latestSheduler: 'local',
+  latestScheduler: 'local',
 
   testingScheduler: {address: null, success: false, detecting: false},
 })
