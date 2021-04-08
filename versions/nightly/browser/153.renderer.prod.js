0e4f3a7fee8282b1ec1b6e7120c6cf04fb4d2282
ci-build <ci-build@fcp-indi.github.com>
2021-04-08 16:49:07 +0000

    updates

--- versions/nightly/browser/153.renderer.prod.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonpCPAC=window.webpackJsonpCPAC||[]).push([[153],{"./node_modules/highlight.js/lib/languages/shell.js":function(s,n){s.exports=function(s){return{name:"Shell Session",aliases:["console"],contains:[{className:"meta",begin:"^\\s{0,3}[/\\w\\d\\[\\]()@-]*[>%$#]",starts:{end:"$",subLanguage:"bash"}}]}}}}]);
--- versions/nightly/browser/54.renderer.prod.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonpCPAC=window.webpackJsonpCPAC||[]).push([[54],{"./node_modules/highlight.js/lib/languages/erb.js":function(n,e){n.exports=function(n){return{name:"ERB",subLanguage:"xml",contains:[n.COMMENT("<%#","%>"),{begin:"<%[%=-]?",end:"[%-]?%>",subLanguage:"ruby",excludeBegin:!0,excludeEnd:!0}]}}}}]);
