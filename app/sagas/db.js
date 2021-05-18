5079e22fd7fd9d10fab567632d3c366ad6646dbc
yiran <yiransucdr@utexas.edu>
2021-05-18 01:26:16 -0500

    tmp: indexedDB solution minor fix

--- app/sagas/db.js
+++ app/sagas/db.js
@@ -6,15 +6,12 @@ db.version(1).stores({cpac: "key,value"})
 
 export async function updateItem(key, value) {
   await db.cpac.put({key: key, value: value})
-  await db.cpac.each(record => console.log("m8", record))
 }
 
 export async function getItem(key) {
-  await db.cpac.each(record => console.log("m9", record))
-  return await db.cpac.where({key: key}).first(val => val ? val.value : null)
+  return db.cpac.where({key: key}).first(val => val ? val.value : null);
 }
 
 export async function clearItem() {
-  await db.cpac.clear()
-  await db.cpac.each(record => console.log("m10", record))
+  await db.cpac.where('key').anyOf('*').delete()
 }
