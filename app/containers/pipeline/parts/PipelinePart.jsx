1247a834fe0f515f3956cc4f8386b5ed3fa55e15
Jon Clucas <jon.clucas@childmind.org>
2021-06-25 11:59:44 -0400

    :recycle: Style dynamic lists

--- app/containers/pipeline/parts/PipelinePart.jsx
+++ app/containers/pipeline/parts/PipelinePart.jsx
@@ -11,6 +11,13 @@ import TextField from '@material-ui/core/TextField';
 import Switch from '@material-ui/core/Switch';
 import InputAdornment from '@material-ui/core/InputAdornment';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+
 import FormGroup from '@material-ui/core/FormGroup';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
@@ -28,6 +35,13 @@ import TableFooter from '@material-ui/core/TableFooter';
 import Help from 'components/Help'
 import FormControlLabelled from 'components/FormControlLabelled'
 import Immutable from 'immutable';
+
+import {
+  AddIcon,
+  EditIcon,
+  DeleteIcon,
+  DuplicateIcon
+} from 'components/icons';
 
 import RoiPaths from './RoiPaths';
 
@@ -83,95 +97,139 @@ function returnComponent(obj, classes={}, onChange=undefined, parents=[], level=
                 <RoiPaths key={entry[0]} configKey={entry[0]} config={entry[1]} onChange={onChange} validOptions={["Avg", "DualReg", "MultReg"]}/>
               )
             default: // all others
-              switch (Immutable.Map.isMap(entry[1])) {
-                case true:
-                  return (
-                    <Accordion key={entry[0]} expanded className={classes.fullWidth}>
-                      <AccordionSummary disabled>
-                        <Typography variant="h6" className={classes.sectionTitle}>
-                          { formatLabel(entry[0]) }
-                        </Typography>
-                      </AccordionSummary>
-                      <AccordionDetails>
-                      <Grid container>
-                        { returnComponent(entry[1], classes, onChange, parents=[...parents.slice(0, level), entry[0]], level + 1) }
-                      </Grid>
-                      </AccordionDetails>
-                    </Accordion>
-                  )
-                case false:
-                  const regex = new RegExp(`^\s*{entry[0]}`);
-                  const label = formatLabel(entry[0]);
-                  const name = [...parents, entry[0]].join('.');
-                  switch (typeof(entry[1])) {
-                    case 'boolean':
-                      return (
-                        <Grid key={entry[0]} item xs={12}>
-                          <FormGroup row>
-                            <Help
-                              type="pipeline"
-                              regex={regex}
-                              help=""
-                            >
-                              <FormControlLabelled label={label}>
-                                <Switch
+              if (Immutable.List.isList(entry[1])) {
+                const regex = new RegExp(`^\s*{entry[0]}`);
+                const label = formatLabel(entry[0]);
+                const name = [...parents, entry[0]].join('.');
+                return (
+                  <Grid key={entry[0]} item xs={12} className={classes.fullWidth}>
+                  <FormGroup row>
+                  <Help
+                    type="pipeline"
+                    regex={regex}
+                    help=""
+                  >
+                    <FormLabel>{label}</FormLabel>
+                  </Help>
+                  <List className={classes.fullWidth}>
+                  {entry[1].map((item, i) => {
+                    return <ListItem button key={i}>
+                    <ListItemText primary={item} className={classes.fullWidth} style={{ padding: '0 115px 0 0'}} />
+                    <ListItemSecondaryAction>
+                      <IconButton onClick={((regi) => () => this.handleEdit(regi))(i)}>
+                        <EditIcon />
+                      </IconButton>
+                      <IconButton onClick={((regi) => () => this.handleDuplicate(regi))(i)}>
+                        <DuplicateIcon />
+                      </IconButton>
+                      <IconButton onClick={((regi) => () => this.handleDelete(regi))(i)}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                    </ListItem>
+                  })}
+                  <Divider />
+                  <ListItem style={{ padding: 10 }}>
+                    <ListItemSecondaryAction>
+                      <IconButton onClick={() => this.handleNew()}>
+                        <AddIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                  </List>
+                  </FormGroup>
+                  </Grid>
+                )
+              } else {
+                switch (Immutable.Map.isMap(entry[1])) {
+                  case true:
+                    return (
+                      <Accordion key={entry[0]} expanded className={classes.fullWidth}>
+                        <AccordionSummary disabled>
+                          <Typography variant="h6" className={classes.sectionTitle}>
+                            { formatLabel(entry[0]) }
+                          </Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                        <Grid container>
+                          { returnComponent(entry[1], classes, onChange, parents=[...parents.slice(0, level), entry[0]], level + 1) }
+                        </Grid>
+                        </AccordionDetails>
+                      </Accordion>
+                    )
+                  case false:
+                    const regex = new RegExp(`^\s*{entry[0]}`);
+                    const label = formatLabel(entry[0]);
+                    const name = [...parents, entry[0]].join('.');
+                    switch (typeof(entry[1])) {
+                      case 'boolean':
+                        return (
+                          <Grid key={entry[0]} item xs={12}>
+                            <FormGroup row>
+                              <Help
+                                type="pipeline"
+                                regex={regex}
+                                help=""
+                              >
+                                <FormControlLabelled label={label}>
+                                  <Switch
+                                    name={name}
+                                    checked={entry[1]}
+                                    onChange={onChange}
+                                    color="primary"
+                                  />
+                                </FormControlLabelled>
+                              </Help>
+                            </FormGroup>
+                          </Grid>
+                        )
+                      case 'string':
+                        return (
+                          <Grid key={entry[0]} item xs={12}>
+                            <FormGroup row>
+                              <Help
+                                type="pipeline"
+                                regex={regex}
+                                help=""
+                                fullWidth >
+                                <TextField
+                                  label={label} fullWidth margin="normal" variant="outlined"
                                   name={name}
-                                  checked={entry[1]}
+                                  value={entry[1]}
                                   onChange={onChange}
-                                  color="primary"
                                 />
-                              </FormControlLabelled>
-                            </Help>
-                          </FormGroup>
-                        </Grid>
-                      )
-                    case 'string':
-                      return (
-                        <Grid key={entry[0]} item xs={12}>
-                          <FormGroup row>
-                            <Help
-                              type="pipeline"
-                              regex={regex}
-                              help=""
-                              fullWidth >
-                              <TextField
-                                label={label} fullWidth margin="normal" variant="outlined"
-                                name={name}
-                                value={entry[1]}
-                                onChange={onChange}
-                              />
-                            </Help>
-                          </FormGroup>
-                        </Grid>
-                      )
-                    case 'number':
-                      return (
-                        <Grid key={entry[0]} item xs={12}>
-                          <FormGroup row>
-                            <Help
-                              type="pipeline"
-                              regex={regex}
-                              help=""
-                              fullWidth >
-                              <TextField
-                                label={label} fullWidth margin="normal" variant="outlined"
-                                name={name}
-                                type='number'
-                                value={entry[1]}
-                                onChange={onChange}
-                              />
-                            </Help>
-                          </FormGroup>
-                        </Grid>
-                      )
-                    default:
-                      // console.log(typeof(entry[1]))
-                      return entry[1]
-                  }
-                default:
-                  return (
-                    <>{ entry[1] }</>
-                  )
+                              </Help>
+                            </FormGroup>
+                          </Grid>
+                        )
+                      case 'number':
+                        return (
+                          <Grid key={entry[0]} item xs={12}>
+                            <FormGroup row>
+                              <Help
+                                type="pipeline"
+                                regex={regex}
+                                help=""
+                                fullWidth >
+                                <TextField
+                                  label={label} fullWidth margin="normal" variant="outlined"
+                                  name={name}
+                                  type='number'
+                                  value={entry[1]}
+                                  onChange={onChange}
+                                />
+                              </Help>
+                            </FormGroup>
+                          </Grid>
+                        )
+                      default:
+                        return entry[1]
+                    }
+                  default:
+                    return (
+                      <>{ entry[1] }</>
+                    )
+                }
               }
           }
         } ) }
